3258adeeeaaeda4d2db6eb39cfb87be72a3fee30
Rahul Ramachandran <rahul.rcd96@gmail.com>
2021-07-01 11:28:13 -0500

    bug fix: add initial and automatic versioning
    version number was set to 6_30_2021 by default.
    now stored in a file called img-version.txt. 
    user asked to enter version number on initial run

--- container-backups.sh
+++ container-backups.sh
@@ -1,18 +1,30 @@
-version="6_30_2021"
+if [ -f "img-version.txt" ]; then
+        echo "Version file exists"
+        read version < img-version.txt
+        echo "Current version number: $version"
+else
+        echo "Version file not found, Please enter a one-time initial image version number:"
+        read init_version
+        echo "Thank you, version stored."
+        echo "$init_version" > img-version.txt
+fi
+
 day=$(date +"%d")
 month=$(date +"%m")
 year=$(date +"%Y")
 new_version="${month}_${day}_${year}"
 
-mkdir container-backups
+mkdir -pv container-backups
 cd container-backups
 
 tt=( $(sudo docker ps --format "{{.Names}}") )
+
 for i in "${tt[@]}"
 do
-	sudo docker rmi ${i/%/:${version}} || true
-	sudo docker commit ${i} ${i/%/:${new_version}}
-	sudo docker save ${i/%/:${new_version}} > ${i}.tar
+        sudo docker rmi ${i/%/:${version}} || true
+        sudo rm ${i/%/_${version}}.tar || true
+        sudo docker commit ${i} ${i/%/:${new_version}}
+        sudo docker save ${i/%/:${new_version}} > ${i/%/_${new_version}}.tar
 done
-version=$new_version
-echo "version = $version"
+echo "$new_version" >  ../img-version.txt
+cd ..
